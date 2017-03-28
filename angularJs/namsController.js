64e8cbbf60c864f754a2fcf081ffcde2872cbbf1
wmy <2980745558@qq.com>
2017-03-28 09:30:06 +0800

    angular1

---
+++ angularJs/namsController.js
@@ -0,0 +1,14 @@
+/**
+ * Created by wmy on 2017/3/27.
+ */
+angular.module("myApp",[]).controller("namesCtrl",function($scope){
+   $scope.names = [
+       {name:"wegt",country:"vehi"},
+       {name:"wegt",country:"vehi"},
+       {name:"wegt",country:"vehi"},
+       {name:"wegt",country:"vehi"},
+       {name:"wegt",country:"vehi"},
+       {name:"wegt",country:"vehi"},
+       {name:"wegt",country:"vehi"}
+   ];
+});
