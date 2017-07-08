da678e4ee30aa53a0779d8cf8d9aac728e2fb46c
wmy <2980745558@qq.com>
2017-07-08 16:49:03 +0800

    project

---
+++ project/library/js/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by wmy on 2017/7/8.
+ */
+$(document).ready(function() {
+    $('#carousel').carousel({
+        interval: 2000
+    })
+});
--- project/meituan/js/index.js
+++ project/meituan/js/index.js
@@ -4,21 +4,50 @@
 $(function () {
     var $guideUl = $("#guide ul");
     var $navSpan = $("#nav span");
+    // $navSpan.on("tap",function () {
+    //     $(this).addClass("active").siblings().removeClass("active");
+    //     $guideUl.css({
+    //         left : -$(this).index()*$guideUl.width()/2
+    //     });
+    // });
+    // $guideUl.on("swipeleft",function () {
+    //     $navSpan.eq(1).addClass("active").siblings().removeClass("active");
+    //     $guideUl.css({
+    //         left:-$guideUl.width()/2
+    //     });
+    // }).on("swiperight",function () {
+    //     $navSpan.eq(0).addClass("active").siblings().removeClass("active");
+    //     $guideUl.css({
+    //             left:0
+    //         });
+    // });
+
     $navSpan.on("tap",function () {
-        $(this).addClass("active").siblings().removeClass("active");
-        $guideUl.css({
-            left : -$(this).index()*$guideUl.width()/2
-        });
+        swipeGuide($(this).index());
     });
     $guideUl.on("swipeleft",function () {
-        $navSpan.eq(1).addClass("active").siblings().removeClass("active");
-        $guideUl.css({
-            left:-$guideUl.width()/2
-        });
+        swipeGuide(1);
     }).on("swiperight",function () {
-        $navSpan.eq(0).addClass("active").siblings().removeClass("active");
-        $guideUl.css({
-                left:0
-            });
+        swipeGuide(0);
     });
+    $("go-top").on("tap",function () {
+        if($(window).scrollTop() <= 1){
+            return false;
+        }
+        $("body").animate({
+            scrollTop:0
+        },500);
+        var top = $(this).css("top");
+        $(this).animate({
+           top :0
+        },500,function(){
+            $(this).css("top",top);
+        });
+    });
+    function swipeGuide(index){
+        $navSpan.eq(index).addClass("active").siblings().removeClass("active");
+       $guideUl.css({
+           left : -index * $guideUl.width()/2
+       });
+    }
 });
